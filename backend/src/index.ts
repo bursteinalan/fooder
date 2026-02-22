933ca44cd0a7d41c49d37ddf632041bee2251ec2
bursteinalan <burstein.alan@gmail.com>
2026-02-22 18:07:07 -0500

    bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -67,7 +67,8 @@ async function startServer() {
     app.use(express.static(frontendPath));
 
     // Serve index.html for all other routes (SPA support)
-    app.get('*', (req, res) => {
+    // Use a wildcard that works with Express 5
+    app.get('/*', (req, res) => {
       const indexPath = path.join(frontendPath, 'index.html');
       if (require('fs').existsSync(indexPath)) {
         res.sendFile(indexPath);
